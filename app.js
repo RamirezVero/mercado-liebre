5229a6ba824789f6e40b94471bcb9fae3054bebd
Veronica <verdani8@gmail.com>
2023-07-25 20:05:40 -0300

    cambio de listen

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const path = require("path");
 app.use(express.static("public"));
 
-app.listen(3000,()=>{
-    console.log("Servidor corriendo en puerto 3000");
-});
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+  })
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
